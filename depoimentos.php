a3bb7ba6bbfb216d02730106e05cc2d01c3dfd7f
marcoprado17 <marco.pdsv@gmail.com>
2017-01-20 20:29:52 -0200

    Adding depositions page

---
+++ depoimentos.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include 'shared-head.php';?>
+	<link rel="stylesheet" href="style/depoimentos.css">
+	<title>Depoimentos | Cultivar Idiomas</title>
+</head>
+<body>
+	<?php include 'header.php';?>
+	<?php include 'nav.php';?>
+
+	<div class="container">
+		<blockquote class="first-blockquote">
+			<div class="blockquote-content-box">
+				<div class="blockquote-img-box blockquote-left-img-box">
+					<img src="img/dina_aquino.jpg" class="img-circle blockquote-img" alt="Foto do aluno 1">
+				</div>
+				<div class="blockquote-text-box">
+					<p>"Recomendo!!! Meu filho estuda na Cultivar e foi avaliado no exterior com nota alta tanto na escrita como na oral!! Fiquei orgulhosa dele e feliz com o curso oferecido pela escola!!! Uma parceria que deu certo e que começa agora com meu outro filho!!"</p>
+				</div>
+			</div>
+			<footer class="blockquote-footer">Dina Aquino - Mãe de aluno</footer>
+		</blockquote>
+
+		<blockquote class="blockquote-reverse">
+			<div class="blockquote-content-box">
+				<div class="blockquote-text-box">
+					<p>"Oi, aqui é o Phellippe, ex aluno da escola. Gostaria de dar um feedback para voces:
+Quando cheguei na Cultivar Escola de Idiomas nunca tinha tido contato com alemão, e muito menos com aulas de uma língua que eu desconhecesse completamente, sempre enfrentei dificuldades na aula de alemão e me perguntava se esse era o caminho. Vocês se propuseram a me ensinar um nível por semestre, então quando vim aqui para a Alemanha estava no meio do B1.2. Chegando aqui fiz uma prova de proficiência elaborada pela faculdade para todos alunos, meu resultado foi B1.2 e por pouco não tirei b2. Acertei 70% das questões que me foram colocadas. Então vocês cumpriram muito bem oque foi acordado! Ao chegas nas aulas de alemão utilizamos outro material, porem a organicidade da aula, e as dificuldades que enfrento aqui são exatamente igual enfrentava ai.
+Então escrevo essa mensagem primeiro p dar o feedback agradecer, pois cheguei aqui com o nível que vocês disseram q eu teria, e também para dizer que as aulas te alemão que tenho aki sao super parecidas, se nao iguais, as aulas que eu tinha ai.
+Muito obrigado por tudo, e sucesso!"</p>
+				</div>
+				<div class="blockquote-img-box">
+					<img src="img/phellippe.jpg" class="img-circle blockquote-img" alt="Foto do aluno 3">
+				</div>
+			</div>
+			<footer class="blockquote-reverse-footer">Phellippe - Ex aluno</footer>
+		</blockquote>
+	</div>
+
+	<?php include 'footer.php';?>
+  <?php include 'shared-scripts.php';?>
+	<script src="js/depoimentos.js"></script>
+</body>
+</html>
---
+++ js/depoimentos.js
@@ -0,0 +1,5 @@
+function main(){
+	$(".depoimentos-nav-btn").addClass("active")
+}
+
+main()
