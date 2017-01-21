6375126afdfcebb9a5a476e5233bf1f8aec3cb4c
marcoprado17 <marco.pdsv@gmail.com>
2017-01-21 00:22:48 -0200

    Implementing navbar correct links

--- nav.php
+++ nav.php
@@ -10,17 +10,17 @@
   <div class="container">
     <div class="navbar-collapse collapse">
       <ul class="nav navbar-nav font-resizeable-nav-bar">
-        <li class="home-nav-btn"><a href="http://cultivari1.uh-app.com.br/">Home</a></li>
-        <li class="quem-somos-nav-btn"><a href="http://cultivari1.uh-app.com.br/quem-somos">Quem Somos</a></li>
-        <li class="cursos-nav-btn"><a href="http://cultivari1.uh-app.com.br/cursos">Cursos</a></li>
-        <li class="depoimentos-nav-btn"><a href="http://cultivari1.uh-app.com.br/depoimentos">Depoimentos</a></li>
-        <li class="escolas-e-localizacao-nav-btn"><a href="http://cultivari1.uh-app.com.br/escolas-e-localizacao">Escolas e Localização</a></li>
-        <li class="contato-nav-btn"><a href="http://cultivari1.uh-app.com.br/contato">Contato</a></li>
+        <li class="home-nav-btn"><a href="/">Home</a></li>
+        <li class="quem-somos-nav-btn"><a href="/quem-somos">Quem Somos</a></li>
+        <li class="cursos-nav-btn"><a href="/cursos">Cursos</a></li>
+        <li class="depoimentos-nav-btn"><a href="/depoimentos">Depoimentos</a></li>
+        <li class="escolas-e-localizacao-nav-btn"><a href="/escolas-e-localizacao">Escolas e Localização</a></li>
+        <li class="contato-nav-btn"><a href="/contato">Contato</a></li>
         <li class="teste-nav-btn dropdown">
-          <a href="http://cultivari1.uh-app.com.br/contato#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Teste de idiomas<span class="caret"></span></a>
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Teste de idiomas<span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="http://cultivari1.uh-app.com.br/teste-ingles">Inglês</a></li>
-            <li><a href="http://cultivari1.uh-app.com.br/teste-alemao">Alemão</a></li>
+            <li><a href="/teste-ingles">Inglês</a></li>
+            <li><a href="/teste-alemao">Alemão</a></li>
           </ul>
         </li>
       </ul>
