086f06e7cfacc32c6b3f5661530772ad4901d501
marcoprado17 <marco.pdsv@gmail.com>
2017-01-21 12:47:53 -0200

    Using contato2 email to send and receive contact email

--- contato-post.php
+++ contato-post.php
@@ -4,8 +4,8 @@
 		$enviaFormularioParaNome = "Cultivar Idiomas Contato";
 		$enviaFormularioParaEmail = "contato2@cultivaridiomas.com.br";
 		 
-		$caixaPostalServidorNome = "Cultivar Idiomas";
-		$caixaPostalServidorEmail = 'no-reply@cultivaridiomas.com.br';
+		$caixaPostalServidorNome = "Cultivar Idiomas Contato";
+		$caixaPostalServidorEmail = 'contato2@cultivaridiomas.com.br';
 		$caixaPostalServidorSenha = 'a2b4c6d8e0';
 		 
 		/*** FIM - DADOS A SEREM ALTERADOS DE ACORDO COM SUAS CONFIGURAÇÕES DE E-MAIL ***/ 
@@ -21,7 +21,7 @@
 		$mensagemConcatenada = 'Formulário de contato gerado'.'<br/>'; 
 		$mensagemConcatenada .= '-------------------------------<br/><br/>'; 
 		$mensagemConcatenada .= 'Nome: '.$remetenteNome.'<br/>'; 
-		$mensagemConcatenada .= 'E-mail: '.$remetenteEmail.'<br/>'; 
+		$mensagemConcatenada .= 'E-mail: '.$remetenteEmail.'<br/><br/>'; 
 		$mensagemConcatenada .= '-------------------------------<br/><br/>'; 
 		$mensagemConcatenada .= 'Mensagem: <br/>';
 		$mensagemConcatenada .= '<div style="white-space: pre-wrap;">';
@@ -53,29 +53,6 @@
 		}else{
 			http_response_code(200);
 		} 
-
-		// $to      = "contato2@cultivaridiomas.com.br";
-		// $subject = "Novo contato do seu site!";
-		// $message = "Olá,\r\n\r\n" . $_POST["name"] . " entrou em contato com você.\r\n" . "E-mail de contato: " . $_POST["email"] . "\r\n\r\nMensagem: " . $_POST["message"];
-		// $message = wordwrap($message, 70);
-
-		// $headers = "From: " . "no-reply@cultivaridiomas.com.br" . PHP_EOL;
-	 //    $headers .= "Reply-To: ". "no-reply@cultivaridiomas.com.br" . PHP_EOL;
-	 //    $headers .= "MIME-Version: 1.0" . PHP_EOL;
-	 //    $headers .= "Content-Type: text/plain; charset=UTF-8" . PHP_EOL;
-
-		// $email_sent_1 = mail($to, $subject, $message, $headers);
-		// $email_sent_2 = mail($to, $subject, $message, $headers);
-		// $email_sent_3 = mail($to, $subject, $message, $headers);
-		// $email_sent_4 = mail($to, $subject, $message, $headers);
-		// $email_sent_5 = mail($to, $subject, $message, $headers);
-
-		// if($email_sent_1 == FALSE and $email_sent_2 == FALSE and $email_sent_3 == FALSE and $email_sent_4 == FALSE and $email_sent_5 == FALSE){
-		// 	http_response_code(400);
-		// }
-		// else {
-		// 	http_response_code(200);
-		// }
 	}
 	else {
 		http_response_code(400);
