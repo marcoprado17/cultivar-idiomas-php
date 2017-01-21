7b58fff38b2a9130e54513d6f11d439543ab6e77
marcoprado17 <marco.pdsv@gmail.com>
2017-01-21 00:01:55 -0200

    Implementing german test

---
+++ teste-alemao.php
@@ -0,0 +1,334 @@
+<?php
+  $idioma='alemão';
+  $questoes=[
+    [
+      //1
+      "pergunta": "a: Guten Tag, ich bin Felix Weber. Und wie _____ Sie?",
+      "cont": "b: Giovanni Mazzini.",
+      "alternativas": [ "heißt", "heißen", "bist", "heiße"],
+      "resposta": 2 //"b"
+    ],
+    [
+      //2
+      "pergunta": "a: _____ kommen Sie, Herr Dupont?",
+      "cont": "b: _____ Frankreich.",
+      "alternativas": ["Wie ... In", "Wer ... Von", "Was ... Bis", "Woher ... Aus"],
+      "resposta": 4 //"d"
+    ],
+    [
+      //3
+      "pergunta": "a: Guten Tag, Frau Selmi. Wie geht es Ihnen?",
+      "cont": "b: Danke, gut. Und _____, Herr Bach?",
+      "alternativas": ["Ihnen", "Sie", "dir", "du"],
+      "resposta": 1 //"a"
+    ],
+      [
+        //4
+      "pergunta": "a: Was sind Sie von Beruf?",
+      "cont": "b: Ich arbeite _____ Journalist.",
+      "alternativas": ["bei", "als", "in", "aus"],
+      "resposta": 2 //"b"
+    ],
+      [
+        //5
+      "pergunta": "a: Wohnt ihr in Mainz?",
+      "cont": "b: Nein, wir _____ in Mainz.",
+      "alternativas": ["lebe nicht", "nicht leben", "wohnen nicht", "nicht wohnen"],
+      "resposta": 3 //"c"
+    ],
+    [
+      //6
+      "pergunta": "a: Welche Sprachen _____ ihr?",
+      "cont": "b: Wir sprechen Italienisch und _____ Deutsch.",
+      "alternativas": ["spreche ... sehr", "sprechen ... ein bisschen", "sprichst ... viel", "sprecht ... ein bisschen"],
+      "resposta": 4 //"d"
+    ],
+    [
+      //7
+      "pergunta": "a: Ist Max dein Bruder?",
+      "cont": "b: _____, Max ist nicht mein Bruder, er ist mein Freund.",
+      "alternativas": ["Nein", "Nicht", "Kein", "Doch"],
+      "resposta": 1 //"a"
+    ],
+    [
+      //8
+      "pergunta": "a: Wer _____ das?",
+      "cont": "b: Das sind _____ Eltern.",
+      "alternativas": ["ist ... meine", "sind ... meine", "sind ... mein", "ist ... sein"],
+      "resposta": 1 //"a"
+    ],
+    [
+      //9
+      "pergunta": "a: Was kostet das Bett?",
+      "cont": "b: ____ kostet 598 €.",
+      "alternativas": ["Er", "Sie", "Es", "Ihr"],
+      "resposta": 3 //"c"
+    ],
+    [
+      //10
+      "pergunta": "a: Brauchen Sie Hilfe?",
+      "cont": "b: _______",
+      "alternativas": ["Ja, sehr.", "Gut, danke.", "Nein, bitte.", "Ja, bitte."],
+      "resposta": 4 //"d"
+    ],
+    [
+      //11
+      "pergunta": "a: Ist das eine Brille?",
+      "cont": "b: Nein, das ist _____ Brille.",
+      "alternativas": ["keine", "nicht", "eine", "ein"],
+      "resposta": 1 //"a"
+    ],
+    [
+      //12
+      "pergunta": "Entschuldigung, _____ heißt das auf Deutsch?",
+      "alternativas": ["warum", "wer", "wie", "wir"],
+      "resposta": 3 //"c"
+    ],
+    [
+      //13
+      "pergunta": "a: Wo ist der Laptop?",
+      "cont": "b: Hier, ich habe _____ Laptop.",
+      "alternativas": ["das", "den", "der", "die"],
+      "resposta": 2 //"b"
+    ],
+    [
+      //14
+      "pergunta": "a: Haben Sie ein Notizbuch?",
+      "cont": "b: Nein, ich habe _____ Notizbuch.",
+      "alternativas": ["keine", "nicht", "kein", "keinen"],
+      "resposta": 3 //"c"
+    ],
+    [
+      //15
+      "pergunta": "a: Hast du alles?",
+      "cont": "b: Nein, ich brauche noch Briefmarke__ und Stift__.",
+      "alternativas": ["-n ... -e", "-- ... -e", "-- ... -s", "-n ... -en"],
+      "resposta": 1 //"a"
+    ],
+    [
+      //16
+      "pergunta": "Du _____ aber gut _____!",
+      "alternativas": ["kannst ... singst", "können ... singen", "kann ... singst", "kannst ... singen"],
+      "resposta": 4 //"d"
+    ],
+    [
+      //17
+      "pergunta": "a: Was sind deine Hobbys?",
+      "cont": "b: Meine Hobbys sind Ski _____ und Musik _____.",
+      "alternativas": ["treffen ... singen", "fahren ... singen", "fahren ... hören", "fahre ... höre"],
+      "resposta": 3 //"c"
+    ],
+    [
+      //18 - (questao 51 do teste 2)
+      "pergunta": "Tim, Johannes, habt ihr ____ Deutschbücher hier vergessen?",
+      "alternativas": ["unsere", "ihre", "eure", "euer"],
+      "resposta": 3 //"c"
+    ],
+    [
+      //19
+      "pergunta": "a: Wann gehst du am Wochenende ins Kino?",
+      "cont": "b: ___ Samstag ___ halb neun. Kommst du mit?",
+      "alternativas": ["Um ... um", "Am ... um", "Von ... bis", "Um ... am"],
+      "resposta": 2 //"b"
+    ],
+    [
+      //20
+      "pergunta": "a: Was möchtet ihr zum Frühstück?",
+      "cont": "b: Wir ____ gern einen Kaffee.",
+      "alternativas": ["möchte", "möchtet", "möchtest", "möchten"],
+      "resposta": 4 //"d"
+    ],
+    [
+      //21
+      "pergunta": "Isst du gern Fisch?",
+      "alternativas": ["Danke, ebenfalls.", "Nein, nicht so gern.", "Bitte, nein!", "Sehr gut, danke."],
+      "resposta": 2 //"b"
+    ],
+    [
+      //22
+      "pergunta": "Holst du mich ab?",
+      "alternativas": ["Der Zug um 10 Uhr ankommt.", "Der Zug ankommen um 10 Uhr.", "Um 10 Uhr ankommt der Zug.", "Der Zug kommt um 10 Uhr an."],
+      "resposta": 4 //"d"
+    ],
+    [
+      //23
+      "pergunta": "Was ____ du gestern ______?",
+      "alternativas": ["habe ... gemacht", "hast ... machen", "hast ... gemacht", "hat ... gemacht"],
+      "resposta": 3 //"c"
+    ],
+    [
+      //24
+      "pergunta": "Ich habe _____ 8.30 Uhr ____ 17 Uhr gearbeitet",
+      "alternativas": ["von ... bis", "um ... am", "am ... um", "bis ... von"],
+      "resposta": 1 //"a"
+    ],
+    [
+      //25
+      "pergunta": "Ich ____ erst um 23 Uhr nach Hause ______",
+      "alternativas": ["bin ... gekommen", "ist ... kommen", "ist ... gekommen", "hat ... gekommen"],
+      "resposta": 1 //"a"
+    ],
+    [
+      //26
+      "pergunta": "Die Polizei ist _____ ___ Post.",
+      "alternativas": ["hinter der", "neben der", "neben die", "zwischen der"],
+      "resposta": 2 //"b"
+    ],
+    [
+      //27
+      "pergunta": "a: Entschuldigung! Ich suche das Hotel Maritim.",
+      "cont": "b: Fahren Sie zuerst _____ (frente) und biegen Sie dann nach ____ (direita) ab",
+      "alternativas": ["geradeaus ... rechts", "zurück ... rechts", "geradeaus ... links", "einen Kilometer ... rechts"],
+      "resposta": 1 //"a"
+    ],
+    [
+      //28
+      "pergunta": "Hier wohnt Maria. Wie findest du _____ Garten?",
+      "alternativas": ["seinen", "ihre", "ihren", "sein"],
+      "resposta": 3 //"c"
+    ],
+    [
+      //29
+      "pergunta": "a: Frauke, Arthur, sind das eure Bücher?",
+      "cont": "b: Nein, sie gehören ___ nicht.",
+      "alternativas": ["dir", "uns", "ihr", "euch"],
+      "resposta": 2 //"b"
+    ],
+    [
+      //30
+      "pergunta": "a: Ich fahre jedes Jahr einmal nach Rom.",
+      "cont": "b: Wirklich? Was ____ __ so gut an der Stadt?",
+      "alternativas": ["gefällst du", "gefällst dir", "gefällt du", "gefällt dir"],
+      "resposta": 4 //"d"
+    ],
+    [
+      //31
+      "pergunta": "Liedeke ist __ ____ ____ Österreich gekommen.",
+      "alternativas": ["vor drei Jahre", "vor drei Jahren", "in drei Jahren", "für drei Jahren"],
+      "resposta": 2 //"b"
+    ],
+    [
+      //32
+      "pergunta": "a: Was willst du später mal werden?",
+      "cont": "b: Ich _____ unbedingt _____.",
+      "alternativas": ["will ... Arzt werden", "werden ... Arzt will", "Arzt ... werden wollen", "Arzt ... will werden"],
+      "resposta": 1 //"a"
+    ],
+    [
+      //33
+      "pergunta": "Ich gehe nie ___ __ Handy aus dem Haus.",
+      "alternativas": ["ohne mein", "mit mein", "ohne meinen", "ohne meinem"],
+      "resposta": 1 //"a"
+    ],
+    [
+      //34
+      "pergunta": "Der Arzt sagt, ich ____ viel Tee trinken.",
+      "alternativas": ["sollst", "sollen", "soll", "sollt"],
+      "resposta": 3 //"c"
+    ],
+    [
+      //35
+      "pergunta": "a: Ich bin krank, aber ich komme trotzdem ins Büro.",
+      "cont": "b: Nein, _____ ___ zu Hause!",
+      "alternativas": ["bleib Sie","bleiben Sie", "Sie sollen", "sollen Sie"],
+      "resposta": 2 //"b"
+    ],
+    [
+      //36
+      "pergunta": "Wie hat dir Melikes Party _____?",
+      "alternativas": ["gefallen", "fallen", "gefunden", "gekommen"],
+      "resposta": 1 //"a"
+    ],
+    [
+      //37
+      "pergunta": "a: Wo ____ ihr gestern?",
+      "cont": "b: Im Büro, wir ____ viel Stress.",
+      "alternativas": ["seid ... haben", "warst ... hatte", "wart ... hatten", "warst ... hattet"],
+      "resposta": 3 //"c"
+    ],
+    [
+      //38
+      "pergunta": "a: Ich bin so müde!",
+      "cont": "b: _____ doch einen Kaffee.",
+      "alternativas": ["Trinkst", "Trink", "Trinkt", "Trinken"],
+      "resposta": 2 //"b"
+    ],
+    [
+      //39
+      "pergunta": "Florian kommt um 18.23 Uhr an. Kannst du ___ am Bahnhof abholen?",
+      "alternativas": ["er", "sie", "es", "ihn"],
+      "resposta": 4 //"d"
+    ],
+    [
+      //40
+      "pergunta": "Hier ____ man nicht rauchen. Das ist verboten!",
+      "alternativas": ["muss", "kann", "darf", "will"],
+      "resposta": 3 //"c"
+    ],
+    [
+      //41
+      "pergunta": "Motorradfahrer _____ immer einen Helm tragen.",
+      "alternativas": ["müssen", "dürfen", "dürfen nicht", "müssen nicht"],
+      "resposta": 1 //"a"
+    ],
+    [
+      //42
+      "pergunta": "Der Rock gefällt mir besser ___ die Mütze. Aber _____ gefällt mir das Kleid.",
+      "alternativas": ["wie ... am meisten", "als ... am besten", "als ... am meisten", "wie ... am besten"],
+      "resposta": 2 //"b"
+    ],
+    [
+      //43
+      "pergunta": "a: Wir spielen Fußball. Kommst du mit?",
+      "cont": "b: Nein, ich sehe ____ fern.",
+      "alternativas": ["gern", "mehr", "am besten", "lieber"],
+      "resposta": 4 //"d"
+    ],
+    [
+      //44
+      "pergunta": "Wir waren gestern im Schwimmbad, denn ______.",
+      "alternativas": ["sehr heiß war es", "sehr heiß es war", "es war sehr heiß", "es sehr heiß war"],
+      "resposta": 3 //"c"
+    ],
+    [
+      //45
+      "pergunta": "Die Sonne scheint, der Himmel ist wolken___.",
+      "alternativas": ["-los", "-bar", "-ig", "-lich"],
+      "resposta": 1 //"a"
+    ],
+    [
+      //46
+      "pergunta": "a: Welches Datum ist heute?",
+      "cont": "b: ___ _____ Januar (7.1.).",
+      "alternativas": ["Der sieben", "Das siebte", "Der siebte", "Der siebente"],
+      "resposta": 3 //"c"
+    ],
+    [
+      //47
+      "pergunta": "Ich habe Geburtstag und ____ gern mit Euch ____. Kommt am Montag um ...",
+      "alternativas": ["würden ... feiern", "würde ... feiere", "würde ... feiern", "würden ... feiere"],
+      "resposta": 3 //"c"
+    ],
+    [
+      //48 - (questao 52 do teste 2)
+      "pergunta": "Kennst du eigentlich schon meine Familie? Auf diesem Foto siehst du unser__ Opa mit sein__ neuen Frau.",
+      "alternativas": ["-n ... -en", "-en ... -en", "-e ... -er", "-en ... -er"],
+      "resposta": 4 //"d"
+    ],
+    [
+      //49 - (questao 53 do teste 2)
+      "pergunta": "a: Wo ist denn das neue Bild?",
+      "cont": "b: Das ____ doch schon über dem Sofa.",
+      "alternativas": ["liegt", "hängt", "steht", "stellt"],
+      "resposta": 2 //"b"
+    ],
+    [
+      //50 - (questao 54 do teste 2)
+      "pergunta": "a: Silke, hast du die neue Lampe gesehen?",
+      "cont": "b: Ja, sie steht _____ Tür. Stell sie doch bitte gleich ____ Tisch.",
+      "alternativas": ["hinter der ... auf den", "hinter die ... auf der", "vor der ... auf die", "an die ... unter den"],
+      "resposta": 1 //"a"
+    ]
+  ];
+  include 'teste.php';
+?>
