2588e742034f693b1b39a86cc66c753ec5a89fe5
marcoprado17 <marco.pdsv@gmail.com>
2017-01-21 14:43:45 -0200

    Temp change

--- contato.php
+++ contato.php
@@ -9,6 +9,8 @@
   <?php include 'header.php';?>
   <?php include 'nav.php';?>
 
+  <?php include "../dados/data.php"; echo "$email"?>
+
   <div class="container">
     <div class="row">
       <div class="col-md-6 contact-message">
