a18b270d59c04c5eb1783c946ce0fbfffc8dfe7b
marcoprado17 <marco.pdsv@gmail.com>
2017-01-21 00:51:06 -0200

    Implementing contact email sent

--- contato-post.php
+++ contato-post.php
@@ -1,11 +1,21 @@
 <?php
-	$to      = 'marco.pdsv@gmail.com';
-	$subject = 'the subject';
-	$message = 'hello';
-	$headers = 'From: webmaster@example.com' . "\r\n" .
-	    'Reply-To: webmaster@example.com' . "\r\n" .
-	    'X-Mailer: PHP/' . phpversion();
+	if( array_key_exists("email", $_POST) and array_key_exists("name", $_POST) and array_key_exists("message", $_POST)){
+		$to      = 'marco.pdsv@gmail.com';
+		$subject = 'Novo contato do seu site!';
+		$message = 'Olá,\n\n' . $_POST["name"] . ' entrou em contato com você.\n' . 'E-mail de contato: ' . $_POST["email"] . '\n\nMensagem: ' . $_POST["message"];
+		$headers = 'From: no-reply@cultivaridiomas.com.br' . "\r\n" .
+		    'X-Mailer: PHP/' . phpversion();
 
-	mail($to, $subject, $message, $headers);
-	http_response_code(200);
+		$email_sent = mail($to, $subject, $message, $headers);
+
+		if($email_sent == FALSE){
+			http_response_code(400);
+		}
+		else {
+			http_response_code(200);
+		}
+	}
+	else {
+		http_response_code(400);
+	}
 ?>
