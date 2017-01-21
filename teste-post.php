c8731add16ecae23bd13a730c8c6b42ab4ed6264
marcoprado17 <marco.pdsv@gmail.com>
2017-01-21 09:58:41 -0200

    Fix

--- teste-post.php
+++ teste-post.php
@@ -2,7 +2,7 @@
 	if( array_key_exists("email", $_POST) and array_key_exists("name", $_POST) and array_key_exists("phone", $_POST) and array_key_exists("correct", $_POST) and array_key_exists("language", $_POST)){
 		$to      = 'marco.pdsv@gmail.com';
 		$subject = 'Novo teste realizado no seu site!';
-		$message = "Olá,\r\n\r\n" . $_POST["name"] . " fez um teste de " . $_POST["language"] . "\r\nE-mail de contato: " . $_POST["email"] . "\n\nTelefone: " . $_POST["phone"] . "\r\n\r\nAcertos: " + $_POST["correct"];
+		$message = "Olá,\r\n\r\n" . $_POST["name"] . " fez um teste de " . $_POST["language"] . "\r\nE-mail de contato: " . $_POST["email"] . "\n\nTelefone: " . $_POST["phone"] . "\r\n\r\nAcertos: " . $_POST["correct"];
 		$headers = 'From: no-reply@cultivaridiomas.com.br' . "\r\n" .
 		    'X-Mailer: PHP/' . phpversion();
 
