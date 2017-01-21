996e3bae111c3e8f8a17c237ca616eca3c57b938
marcoprado17 <marco.pdsv@gmail.com>
2017-01-21 00:11:16 -0200

    Fix

--- teste-ingles.php
+++ teste-ingles.php
@@ -3,303 +3,303 @@
   $questoes=[
     [
       //1
-      "These are your books, _____ they?",
-      [ "isn't", "aren't", "weren't"],
-      2 //"b"
+      "pergunta": "These are your books, _____ they?",
+      "alternativas": [ "isn't", "aren't", "weren't"],
+      "resposta": 2 //"b"
     ],
     [
       //2
-      "I told the teacher _____ I had seen.",
-      [ "what", "which", "where"],
-      1 //"a"
+      "pergunta": "I told the teacher _____ I had seen.",
+      "alternativas": [ "what", "which", "where"],
+      "resposta": 1 //"a"
     ],
     [
       //3
-      "We talked about _____ new words.",
-      [ "learn", "learned", "learning"],
-      3 //"c"
+      "pergunta": "We talked about _____ new words.",
+      "alternativas": [ "learn", "learned", "learning"],
+      "resposta": 3 //"c"
     ],
-    [
-      //4
-      "A lot of rice is grown in Asia.\nThe sentence is mostly about _____.",
-      [ "why people grow rice", "how people grow rice", "where people grow rice"],
-      3 //"c"
+      [
+        //4
+      "pergunta": "A lot of rice is grown in Asia.\nThe sentence is mostly about _____.",
+      "alternativas": [ "why people grow rice", "how people grow rice", "where people grow rice"],
+      "resposta": 3 //"c"
     ],
-    [
-      //5
-      "I asked a woman when I could catch the Broadway bus. She said, \"The Broadway bus arrives at six o'clock.\" The Broadway bus_____.",
-      [ "will arrive in the future", "has already arrived", "won't be arriving"],
-      1 //"a"
+      [
+        //5
+      "pergunta": "I asked a woman when I could catch the Broadway bus. She said, \"The Broadway bus arrives at six o'clock.\" The Broadway bus_____.",
+      "alternativas": [ "will arrive in the future", "has already arrived", "won't be arriving"],
+      "resposta": 1 //"a"
     ],
     [
       //6
-      "I would travel to Hawaii if I _____ a lot of money in the lottery.",
-      ["get", "won", "will"],
-      2 //"b"
+      "pergunta": "I would travel to Hawaii if I _____ a lot of money in the lottery.",
+      "alternativas": ["get", "won", "will"],
+      "resposta": 2 //"b"
     ],
     [
       //7
-      "This is the dress _____ my grandmother made.",
-      ["whom", "where", "which"],
-      3 //"c"
+      "pergunta": "This is the dress _____ my grandmother made.",
+      "alternativas": ["whom", "where", "which"],
+      "resposta": 3 //"c"
     ],
     [
       //8
-      "\"Carl would rather not go to the party.\"\nThe speaker means _____.",
-      ["Carl really wants to go.", "Carl doesn't want to go.", "Carl wishes he could go."],
-      2 //"b"
+      "pergunta": "\"Carl would rather not go to the party.\"\nThe speaker means _____.",
+      "alternativas": ["Carl really wants to go.", "Carl doesn't want to go.", "Carl wishes he could go."],
+      "resposta": 2 //"b"
     ],
     [
       //9
-      "Sara _____ to Los Angeles in 2005.",
-      ["moved", "moving", "has moved"],
-      1 //"a"
+      "pergunta": "Sara _____ to Los Angeles in 2005.",
+      "alternativas": ["moved", "moving", "has moved"],
+      "resposta": 1 //"a"
     ],
     [
       //10
-      "If I have enough time, I _____ help you with your homework.",
-      ["am", "will", "would"],
-      2 //"b"
+      "pergunta": "If I have enough time, I _____ help you with your homework.",
+      "alternativas": ["am", "will", "would"],
+      "resposta": 2 //"b"
     ],
     [
       //11
-      "Right now, it _____.",
-      ["rain", "rains", "is raining"],
-      3 //"c"
+      "pergunta": "Right now, it _____.",
+      "alternativas": ["rain", "rains", "is raining"],
+      "resposta": 3 //"c"
     ],
     [
       //12
-      "This pen is _____ same as that one.",
-      ["as", "not", "the"],
-      3 //"c"
+      "pergunta": "This pen is _____ same as that one.",
+      "alternativas": ["as", "not", "the"],
+      "resposta": 3 //"c"
     ],
     [
       //13
-      "\"Do you think it will rain?\"\nThe speaker wants to know about _____.",
-      ["the weather now", "the weather in the past", "the weather in the future"],
-      3 //"c"
+      "pergunta": "\"Do you think it will rain?\"\nThe speaker wants to know about _____.",
+      "alternativas": ["the weather now", "the weather in the past", "the weather in the future"],
+      "resposta": 3 //"c"
     ],
     [
       //14
-      "She doesn't want _____ coffee.",
-      ["any", "some", "many"],
-      1 //"a"
+      "pergunta": "She doesn't want _____ coffee.",
+      "alternativas": ["any", "some", "many"],
+      "resposta": 1 //"a"
     ],
     [
       //15
-      "\"Stephanie will regret her decision sooner or later.\"\nThe speaker means Stephanie _____.",
-      ["might make a decision soon.", "could be happier later.", "is definitely going to be sorry."],
-      3 //"c"
+      "pergunta": "\"Stephanie will regret her decision sooner or later.\"\nThe speaker means Stephanie _____.",
+      "alternativas": ["might make a decision soon.", "could be happier later.", "is definitely going to be sorry."],
+      "resposta": 3 //"c"
     ],
     [
       //16
-      "All of the food _____ on the table.",
-      ["is", "am", "are"],
-      1 //"a"
+      "pergunta": "All of the food _____ on the table.",
+      "alternativas": ["is", "am", "are"],
+      "resposta": 1 //"a"
     ],
     [
       //17
-      "\"We ran out of milk.\"\nThe speaker means we have _____.",
-      ["no milk", "some milk", "a lot of milk"],
-      1 //"a"
+      "pergunta": "\"We ran out of milk.\"\nThe speaker means we have _____.",
+      "alternativas": ["no milk", "some milk", "a lot of milk"],
+      "resposta": 1 //"a"
     ],
     [
       //18
-      "Robert _____ his keys in the living room.",
-      ["find", "found", "founded"],
-      2 //"b"
+      "pergunta": "Robert _____ his keys in the living room.",
+      "alternativas": ["find", "found", "founded"],
+      "resposta": 2 //"b"
     ],
     [
       //19
-      "Don and Nancy _____ the house yesterday.",
-      ["cleaned", "cleaning", "cleaner"],
-      1 //"a"
+      "pergunta": "Don and Nancy _____ the house yesterday.",
+      "alternativas": ["cleaned", "cleaning", "cleaner"],
+      "resposta": 1 //"a"
     ],
     [
       //20
-      "Leah said, \"I'm bored with this book. Let's watch TV.\"\nWhy did Leah want to watch TV?",
-      ["The TV wasn't interesting.", "Leah wasn't interesting.", "The book wasn't interesting."],
-      3 //"c"
+      "pergunta": "Leah said, \"I'm bored with this book. Let's watch TV.\"\nWhy did Leah want to watch TV?",
+      "alternativas": ["The TV wasn't interesting.", "Leah wasn't interesting.", "The book wasn't interesting."],
+      "resposta": 3 //"c"
     ],
     [
       //21
-      "We _____ to have a test next Friday.",
-      ["are going", "may going", "will going"],
-      1 //"a"
+      "pergunta": "We _____ to have a test next Friday.",
+      "alternativas": ["are going", "may going", "will going"],
+      "resposta": 1 //"a"
     ],
     [
       //22
-      "\"You lost the tennis match, didn't you?\"\nThe speaker thinks you _____.",
-      ["didn't lose the match", "probably lost the match", "will most likely lose the match"],
-      2 //"b"
+      "pergunta": "\"You lost the tennis match, didn't you?\"\nThe speaker thinks you _____.",
+      "alternativas": ["didn't lose the match", "probably lost the match", "will most likely lose the match"],
+      "resposta": 2 //"b"
     ],
     [
       //23
-      "Dave and Chris _____ very early tomorrow morning.",
-      ["will leaving", "are leaving", "have left"],
-      2 //"b"
+      "pergunta": "Dave and Chris _____ very early tomorrow morning.",
+      "alternativas": ["will leaving", "are leaving", "have left"],
+      "resposta": 2 //"b"
     ],
     [
       //24
-      "_____ a new language is never easy.",
-      ["Learn", "Learned", "Learning"],
-      3 //"c"
+      "pergunta": "_____ a new language is never easy.",
+      "alternativas": ["Learn", "Learned", "Learning"],
+      "resposta": 3 //"c"
     ],
     [
       //25
-      "When Larry arrived, the family sat down to eat. Which event happened first?",
-      ["The family sat down.", "Everyone ate.", "Larry arrived."],
-      3 //"c"
+      "pergunta": "When Larry arrived, the family sat down to eat. Which event happened first?",
+      "alternativas": ["The family sat down.", "Everyone ate.", "Larry arrived."],
+      "resposta": 3 //"c"
     ],
     [
       //26
-      "The laundry was _____ by my sister.",
-      ["do", "done", "doing"],
-      2 //"b"
+      "pergunta": "The laundry was _____ by my sister.",
+      "alternativas": ["do", "done", "doing"],
+      "resposta": 2 //"b"
     ],
     [
       //27
-      "I won't play basketball with Pete because he never _____ the rules.",
-      ["attends", "follows", "targets"],
-      2 //"b"
+      "pergunta": "I won't play basketball with Pete because he never _____ the rules.",
+      "alternativas": ["attends", "follows", "targets"],
+      "resposta": 2 //"b"
     ],
     [
       //28
-      "The teacher is writing the answers on the board.\nThe sentence is about _____.",
-      ["the past", "the present", "the future"],
-      2 //"b"
+      "pergunta": "The teacher is writing the answers on the board.\nThe sentence is about _____.",
+      "alternativas": ["the past", "the present", "the future"],
+      "resposta": 2 //"b"
     ],
     [
       //29
-      "Greg is taller than Samuel.\nWhich sentence could be true?",
-      ["Greg is 181 cm tall, and Samuel is 176 cm tall.", "Greg is 176 cm tall, and Samuel is 181 cm tall.", "Greg is 181 cm tall, and Samuel is 181 cm tall."],
-      1 //"a"
+      "pergunta": "Greg is taller than Samuel.\nWhich sentence could be true?",
+      "alternativas": ["Greg is 181 cm tall, and Samuel is 176 cm tall.", "Greg is 176 cm tall, and Samuel is 181 cm tall.", "Greg is 181 cm tall, and Samuel is 181 cm tall."],
+      "resposta": 1 //"a"
     ],
     [
       //30
-      "Hana got 89% on the test. John got 96% on the test. Rita got 78% on the test. John got _____ score on the test.",
-      ["the higher", "the highest", "the most high"],
-      2 //"b"
+      "pergunta": "Hana got 89% on the test. John got 96% on the test. Rita got 78% on the test. John got _____ score on the test.",
+      "alternativas": ["the higher", "the highest", "the most high"],
+      "resposta": 2 //"b"
     ],
     [
       //31
-      "Before you travel by train, you need to buy a _____.",
-      ["travel agent", "reservation", "ticket"],
-      3 //"c"
+      "pergunta": "Before you travel by train, you need to buy a _____.",
+      "alternativas": ["travel agent", "reservation", "ticket"],
+      "resposta": 3 //"c"
     ],
     [
       //32
-      "A _____ is a kind of vegetable.",
-      ["strawberry", "wheat", "carrot"],
-      3 //"c"
+      "pergunta": "A _____ is a kind of vegetable.",
+      "alternativas": ["strawberry", "wheat", "carrot"],
+      "resposta": 3 //"c"
     ],
     [
       //33
-      "\"The airplane is crowded.\"\nThe speaker means the airplane is _____.",
-      ["quite new", "too full", "very late"],
-      2 //"b"
+      "pergunta": "\"The airplane is crowded.\"\nThe speaker means the airplane is _____.",
+      "alternativas": ["quite new", "too full", "very late"],
+      "resposta": 2 //"b"
     ],
     [
       //34
-      "People usually touch something with their _____.",
-      ["eyes", "noses", "fingers"],
-      3 //"c"
+      "pergunta": "People usually touch something with their _____.",
+      "alternativas": ["eyes", "noses", "fingers"],
+      "resposta": 3 //"c"
     ],
     [
       //35
-      "Melanie said to Grace, \"The blue dictionary is mine.\"\nMelanie means the dictionary belongs to _____.",
-      ["Melanie", "Grace", "Melanie and Grace"],
-      1 //"a"
+      "pergunta": "Melanie said to Grace, \"The blue dictionary is mine.\"\nMelanie means the dictionary belongs to _____.",
+      "alternativas": ["Melanie", "Grace", "Melanie and Grace"],
+      "resposta": 1 //"a"
     ],
     [
       //36
-      "There _____ two lamps in the bedroom.",
-      ["is", "are", "was"],
-      2 //"b"
+      "pergunta": "There _____ two lamps in the bedroom.",
+      "alternativas": ["is", "are", "was"],
+      "resposta": 2 //"b"
     ],
     [
       //37
-      "_____ he goes is not my problem.",
-      ["Who", "What", "Where"],
-      3 //"c"
+      "pergunta": "_____ he goes is not my problem.",
+      "alternativas": ["Who", "What", "Where"],
+      "resposta": 3 //"c"
     ],
     [
       //38
-      "Bill's class, which meets at 11:00, has 28 students in it. Bill has _____.",
-      ["one class", "at least two classes", "three or more classes"],
-      1 //"a"
+      "pergunta": "Bill's class, which meets at 11:00, has 28 students in it. Bill has _____.",
+      "alternativas": ["one class", "at least two classes", "three or more classes"],
+      "resposta": 1 //"a"
     ],
     [
       //39
-      "Patty and Lou went to India.\nThe sentence means _____.",
-      ["Only Patty went.", "They both went", "Lou went alone"],
-      2 //"b"
+      "pergunta": "Patty and Lou went to India.\nThe sentence means _____.",
+      "alternativas": ["Only Patty went.", "They both went", "Lou went alone"],
+      "resposta": 2 //"b"
     ],
     [
       //40
-      "What time _____ the class begin?",
-      ["does", "was", "is"],
-      1 //"a"
+      "pergunta": "What time _____ the class begin?",
+      "alternativas": ["does", "was", "is"],
+      "resposta": 1 //"a"
     ],
     [
       //41
-      "The children _____ in the classroom.",
-      ["is", "am", "are"],
-      3 //"c"
+      "pergunta": "The children _____ in the classroom.",
+      "alternativas": ["is", "am", "are"],
+      "resposta": 3 //"c"
     ],
     [
       //42
-      "\"The situation has really gotten out of hand.\"\nThe speaker means the situation is _____.",
-      ["not easy to understand", "not under control", "not beneficial"],
-      2 //"b"
+      "pergunta": "\"The situation has really gotten out of hand.\"\nThe speaker means the situation is _____.",
+      "alternativas": ["not easy to understand", "not under control", "not beneficial"],
+      "resposta": 2 //"b"
     ],
     [
       //43
-      "My father enjoys _____ golf.",
-      ["plays", "playing", "to play"],
-      2 //"b"
+      "pergunta": "My father enjoys _____ golf.",
+      "alternativas": ["plays", "playing", "to play"],
+      "resposta": 2 //"b"
     ],
     [
       //44
-      "The class hasn'’'t ended _____.",
-      ["yet", "now", "already"],
-      1 //"a"
+      "pergunta": "The class hasn'’'t ended _____.",
+      "alternativas": ["yet", "now", "already"],
+      "resposta": 1 //"a"
     ],
     [
       //45
-      "If you cope with something, you _____ with it.",
-      ["play", "deal", "tend"],
-      2 //"b"
+      "pergunta": "If you cope with something, you _____ with it.",
+      "alternativas": ["play", "deal", "tend"],
+      "resposta": 2 //"b"
     ],
     [
       //46
-      "Remember to send _____ an email.",
-      ["her", "she", "hers"],
-      1 //"a"
+      "pergunta": "Remember to send _____ an email.",
+      "alternativas": ["her", "she", "hers"],
+      "resposta": 1 //"a"
     ],
     [
       //47
-      "Adele exercises _____ healthy.",
-      ["to be", "was", "are"],
-      1 //"a"
+      "pergunta": "Adele exercises _____ healthy.",
+      "alternativas": ["to be", "was", "are"],
+      "resposta": 1 //"a"
     ],
     [
       //48
-      "Could you please tell me _____?",
-      ["where is the Grand Hotel", "is where the Grand Hotel", "where the Grand Hotel is"],
-      3 //"c"
+      "pergunta": "Could you please tell me _____?",
+      "alternativas": ["where is the Grand Hotel", "is where the Grand Hotel", "where the Grand Hotel is"],
+      "resposta": 3 //"c"
     ],
     [
       //49
-      "\"Can you give me a hand?\"\nThe speaker wants _____.",
-      ["help", "praise", "money"],
-      1 //"a"
+      "pergunta": "\"Can you give me a hand?\"\nThe speaker wants _____.",
+      "alternativas": ["help", "praise", "money"],
+      "resposta": 1 //"a"
     ],
     [
       //50
-      "Neither Mark nor Alex knows the answer.\nThe sentence means _____.",
-      ["Mark doesn'’'t know the answer, and Alex doesn'’'t know the answer.", "Mark knows the answer, but Alex doesn'’'t know it.", "Mark and Alex both know the answer."],
-      1 //"a"
+      "pergunta": "Neither Mark nor Alex knows the answer.\nThe sentence means _____.",
+      "alternativas": ["Mark doesn'’'t know the answer, and Alex doesn'’'t know the answer.", "Mark knows the answer, but Alex doesn'’'t know it.", "Mark and Alex both know the answer."],
+      "resposta": 1 //"a"
     ]
   ];
   include 'teste.php';
